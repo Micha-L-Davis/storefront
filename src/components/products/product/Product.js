3ebb66999ede61ac6fd3023e91084a1ad8f00247
Micha Davis <m.leigh.davis@gmail.com>
2022-05-10 22:26:19 -0700

    add event handler for buy button

--- src/components/products/product/Product.js
+++ src/components/products/product/Product.js
@@ -1,15 +1,27 @@
 import { Box, Button } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { addToCart, takeFromStock } from "../../../store/actions";
 
 export default function Product(props) {
+  const dispatch = useDispatch();
+
+  const handleBuyClick = (product) => {
+    let addAction = addToCart(product);
+    dispatch(addAction);
+
+    let takeAction = takeFromStock(product);
+    dispatch(takeAction);
+  }
+
   return (
     <Box sx={{
       minHeight: '100px',
       minWidth: '100px'
     }}>
       <img src="https://via.placeholder.com/200" alt="placeholder"></img>
-      <p>{props.product.name}</p>
+      <p>{props.product.displayName}</p>
       <p>{props.product.description}</p>
-      <Button>Buy Now</Button>
+      <Button onClick={() => handleBuyClick(props.product)}>Buy Now</Button>
       <Button>Details</Button>
     </Box>
   );
