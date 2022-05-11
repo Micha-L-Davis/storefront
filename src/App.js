308709ed3dcdded999e944aae3baae1f731c8ce3
Micha Davis <m.leigh.davis@gmail.com>
2022-05-11 16:25:14 -0700

    add useEffect hook to get initial state from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Categories from './components/categories/Categories';
 import Products from './components/products/Products';
 import SimpleCart from './components/simple-cart/SimpleCart';
+import { getProducts } from './store/products';
+import { getCategories } from './store/categories';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+    dispatch(getProducts());
+  }, [])
+
   return (
     <div className="App">
       <Header />
