4d7b82430f7538e18d87a01476722076bb0c01b3
Micha Davis <m.leigh.davis@gmail.com>
2022-05-10 22:28:58 -0700

    add normalized name and change old name to display name

--- src/store/products.js
+++ src/store/products.js
@@ -2,42 +2,48 @@ const initialState = {
   products: [
     {
       category: 'books',
-      name: 'Lulu and Muffin #1',
+      name: 'lulu-and-muffin',
+      displayName: 'Lulu and Muffin #1',
       description: 'The Springdale Sanctuary is home and refuge for the unfortunate animals who lack the skills to be returned to the wild. Lulu, a hyrax, and her penmate Muffin, a koala, must face a tyrannical turtle, malcontent monkeys, pesky patrons and a watchful warden on their quest to find identity and a meaningful place in an artificial world designed for their protection. 16 pages, full color.',
       price: '$4',
       inventoryCount: 228
     },
     {
       category: 'books',
-      name: 'Be Curious - A Comics Anthology',
+      name: 'be-curious',
+      displayName: 'Be Curious - A Comics Anthology',
       description: 'An anthology showcasing the past 10 years of my comics and illustrations. It captures my growth as an artist and highlights the themes which have preoccupied me over that span. 48 pages, b&w.',
       price: '$11',
       inventoryCount: 245
     },
     {
       category: 'prints',
-      name: 'Seek Tranquility',
+      name: 'seek-tranquility',
+      displayName: 'Seek Tranquility',
       description: 'An illustration of the lunar lander at Tranquility Base.',
       price: '$15',
       inventoryCount: 113
     },
     {
       category: 'prints',
-      name: 'Flight of the Barn Owl',
+      name: 'barn-owl',
+      displayName: 'Flight of the Barn Owl',
       description: 'A digital painting of a barn owl flying over a field of dry grass.',
       price: '$15',
       inventoryCount: 87
     },
     {
       category: 'prints',
-      name: 'Be Curious',
-      description: 'An illustration of the Curiosity Mars rover.',
+      name: 'curious-cover',
+      displayName: 'Be Curious',
+      description: 'An illustration of the Curiosity Mars rover, featured on the cover of the book by the same name.',
       price: '$15',
       inventoryCount: 109
     },
     {
       category: 'prints',
-      name: '... Wins the Race',
+      name: 'race',
+      displayName: '... Wins the Race',
       description: 'A whimsical re-imagining of a classic fable.',
       price: '$15',
       inventoryCount: 61
@@ -47,21 +53,23 @@ const initialState = {
 }
 
 function productsReducer(state = initialState, action) {
+  let productList = [...state.products, ...state.hiddenProducts]
   switch (action.type) {
     case 'FILTER':
-      let productList = [...state.products, ...state.hiddenProducts]
-      let showList = productList.filter(product => product.category === action.payload);
-      let hideList = productList.filter(product => product.category !== action.payload);
+      let showList = productList.filter(product => product.category === action.payload && product.inventoryCount > 0);
+      let hideList = productList.filter(product => product.category !== action.payload || product.inventoryCount < 1);
       return { ...state, products: showList, hiddenProducts: hideList };
+    case 'VEND':
+      let productToVend = productList.filter(product => product.name === action.payload);
+      productToVend.inventoryCount--;
+      if (productToVend.inventoryCount < 0) productToVend.inventoryCount = 0;
+      return state;
+    case 'RESTOCK':
+      let productToRestock = productList.filter(product => product.name === action.payload);
+      productToRestock.inventoryCount++;
+      return state;
     default:
       return state;
-  }
-}
-
-export const filterProducts = (category) => {
-  return {
-    type: 'FILTER',
-    payload: category
   }
 }
 
